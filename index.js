b6d4c38eb065ba4a9dd8d2a16cdee030b0e6d5b4
Jose Gascon <PiiXiieeS@users.noreply.github.com>
2021-03-25 21:48:13 +0100

    feat: updated version that uses ES6 crypto API
    Credits for https://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 "use strict";
 
 function uuid() {
-  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
-    var r = (Math.random() * 16) | 0;
-    var v = c === "x" ? r : (r & 0x3) | 0x8;
-
-    return v.toString(16);
-  });
+  return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+  );
 }
 
 module.exports = uuid;
