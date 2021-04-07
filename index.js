80648779e0b46b2613174fa89bea3cd7dcef2dd8
Jose Gascon <PiiXiieeS@users.noreply.github.com>
2021-04-07 23:28:38 +0200

    fix(uuid): remove arrow function
    move from arrow function to standard function(c) to avoid issues with minifying with gulp task

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 "use strict";
+// https://stackoverflow.com/a/2117523/2601552
 
 function uuid() {
-  return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
-    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-  );
+  return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, function (c) {
+    return (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)}
+  )
 }
 
 module.exports = uuid;
