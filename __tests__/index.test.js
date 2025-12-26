93819181663b7f920304cd5be1e7edbd5a0fa8fb
Adriana Machado <adrmachadopaypal@gmail.com>
2025-12-26 16:04:18 +0000

    update dependencies

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,16 +1,6 @@
 const uuid = require("../");
 const isUuid = require("is-uuid");
 
-const crypto = require("crypto");
-
-// jest does not have crypto defined
-// https://stackoverflow.com/a/52612372/2601552
-Object.defineProperty(global.self, "crypto", {
-  value: {
-    getRandomValues: (arr) => crypto.randomBytes(arr.length),
-  },
-});
-
 describe("uuid", () => {
   it("returns valid v4 UUIDs", () => {
     let i;
