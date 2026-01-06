c2199cd535d7614ae1480b70b4689bfabc4e8cc7
Viacheslav Anosov <infamexs@gmail.com>
2026-01-06 15:26:16 -0600

    make compatible with es5

--- index.js
+++ index.js
@@ -12,14 +12,14 @@ function uuid() {
 
   // Browser Web Crypto API
   if (typeof crypto !== 'undefined' && typeof crypto.getRandomValues === 'function') {
-    const bytes = new Uint8Array(16);
+    var bytes = new Uint8Array(16);
     crypto.getRandomValues(bytes);
 
     bytes[6] = (bytes[6] & 0x0f) | 0x40;
     bytes[8] = (bytes[8] & 0x3f) | 0x80;
 
-    const hex = new Array(256);
-    for (let i = 0; i < 256; i++) hex[i] = (i < 16 ? "0" : "") + i.toString(16);
+    var hex = new Array(256);
+    for (var i = 0; i < 256; i++) hex[i] = (i < 16 ? "0" : "") + i.toString(16);
 
     return (
       hex[bytes[0]] + hex[bytes[1]] + hex[bytes[2]] + hex[bytes[3]] + "-" +
