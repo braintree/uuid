3d71484998faa305d57b6815b557f8c294fa6c9a
Ludo van den Boom <lvandenboom@paypal.com>
2026-04-03 09:17:02 -0500

    Scope loop variable i inside for loop initializer

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -13,9 +13,7 @@ describe("uuid", () => {
   });
 
   it("returns valid v4 UUIDs", () => {
-    let i;
-
-    for (i = 0; i < 10; i++) {
+    for (let i = 0; i < 10; i++) {
       expect(isUuid.v4(uuid())).toBe(true);
     }
   });
